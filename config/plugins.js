f440a17075b976c740d2f6dd181f3ca4530c9f28
jinzhu <nanjinzhu666@gmail.com>
2020-12-05 21:59:46 -0500

    Initail Commit

---
+++ config/plugins.js
@@ -0,0 +1,22 @@
+module.exports=({ env })=>   ({ 
+    // ...
+    upload:{ 
+      provider: "cloudinary",
+      providerOptions:{ 
+        cloud_name:env('CLOUDINARY_NAME'), 
+        api_key:env('CLOUDINARY_KEY'), 
+        api_secret: env('CLOUDINARY_SECRET')
+      }
+    },
+    email: {
+        provider: "nodemailer-v3",
+        providerOptions: {},
+        settings: {
+            host: 'smtp.gmail.com',
+            port: 587,
+            username: 'nanjinzhu202012@gmail.com',
+            password: 'woshinjz',
+            secure: false
+        }
+    }
+  });
