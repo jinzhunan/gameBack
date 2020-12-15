c68a52f604c0d489a108e0fab96786cdfd3430e2
jinzhu <nanjinzhu666@gmail.com>
2020-12-15 00:44:20 -0500

    make it better

--- api/memory-game/controllers/memory-game.js
+++ api/memory-game/controllers/memory-game.js
@@ -1,9 +1,18 @@
 const { sanitizeEntity,parseMultipartData } = require('strapi-utils');
+var cloudinary = require('cloudinary').v2;
+// var streamifier = require('streamifier') 
+
+
+cloudinary.config({ 
+    cloud_name: 'dhhuemjhf', 
+    api_key: '717721136637757', 
+    api_secret: 'h3urE5uA-V-7sffed06oBL1qsH8' 
+  });
 
 
 module.exports = {
 
-  async create(ctx) {
+    async create(ctx) {
 
       let entity;
       if (ctx.is('multipart')) {
@@ -31,9 +40,62 @@ module.exports = {
         }
     
         return sanitizeEntity(entity, { model: strapi.models['memory-game'] });
-      },
+    },
     
+    async delete(ctx) {
+        const { id } = ctx.params;
+        console.log(id)
+        // findOne by id
+        const entity = await strapi.services['memory-game'].findOne({ id });
+        // console.log(entity)
 
+        if(entity.cover.mime.includes('video')){
+            const FilePublicId = entity.cover.url.split('/')[7].split('.')[0]
+            // delete cloudnary
+            cloudinary.uploader.destroy(FilePublicId, {resource_type: 'video'}, 
+            function(error,result) { console.log(result, error) });
+        }
+        if(entity.cover.mime.includes('audio')){
+            const FilePublicIdAudio = entity.cover.url.split('/')[7].split('.')[0]
+            const FilePublicIdImage = entity.cover2.url.split('/')[7].split('.')[0]
+            // delete audio
+            cloudinary.uploader.destroy(FilePublicIdAudio, {resource_type: 'video'}, 
+            function(error,result) { console.log(result, error) });
+            // delete imgs
+            const thumbnal = `thumbnail_${FilePublicIdImage}`
+            const large = `large_${FilePublicIdImage}`
+            const medium = `medium_${FilePublicIdImage}`
+            const small = `small_${FilePublicIdImage}`
+            const urls = [FilePublicIdImage,thumbnal,large,medium,small]
+            urls.forEach(url => {
+                // delete cloudnary
+                cloudinary.uploader.destroy(url, {resource_type: 'image'}, 
+                function(error,result) { console.log(result, error) });
+            })
+        }
+
+        if(entity.cover.mime.includes('image')){
+            const FilePublicId = entity.cover.url.split('/')[7].split('.')[0]
+            const thumbnal = `thumbnail_${FilePublicId}`
+            const large = `large_${FilePublicId}`
+            const medium = `medium_${FilePublicId}`
+            const small = `small_${FilePublicId}`
+            console.log(FilePublicId)
+            const urls = [FilePublicId,thumbnal,large,medium,small]
+            urls.forEach(url => {
+                // delete cloudnary
+                cloudinary.uploader.destroy(url, {resource_type: 'image'}, 
+                function(error,result) { console.log(result, error) });
+            })
+
+        }
+
+
+        // delete model
+        const entity = await strapi.services.restaurant.delete({ id });
+
+        return sanitizeEntity(entity, { model: strapi.models.restaurant });
+    },
 }
 
 
