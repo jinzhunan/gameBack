0f2a2b20d3403b4fdb2be1afcb71f367ac3afc82
jinzhu <nanjinzhu666@gmail.com>
2020-12-26 17:01:06 -0500

    make it better

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,5 @@
   module.exports=({ env })=>   ({ 
-    upload:{ 
-      provider: "cloudinary",
-      providerOptions:{ 
-        cloud_name:'dhhuemjhf',
-        api_key:'717721136637757',
-        api_secret: 'h3urE5uA-V-7sffed06oBL1qsH8'
-      }
-    },
+
     email: {
         provider: "nodemailer-v3",
         providerOptions: {},
